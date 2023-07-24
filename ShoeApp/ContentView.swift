2819a0b9e2c516834688158d3d65360f25d53d2a
teja <tejagoud.ios@gmail.com>
2023-07-24 20:10:35 +1000

    Initial Commit

---
+++ ShoeApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ShoeApp
+//
+//  Created by TejaDanasvi on 24/7/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ShoeApp/ShoeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoeAppApp.swift
+//  ShoeApp
+//
+//  Created by TejaDanasvi on 24/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct ShoeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
