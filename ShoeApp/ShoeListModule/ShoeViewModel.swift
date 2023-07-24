7944c21ad9a36eb103b2a636ffb8a9852b5e9c1d
teja <tejagoud.ios@gmail.com>
2023-07-24 20:34:01 +1000

    Added in a folder called ShoeListModule

---
+++ ShoeApp/ShoeListModule/ShoeViewModel.swift
@@ -0,0 +1,43 @@
+//
+//  ShoeViewModel.swift
+//  ShoeApp
+//
+//  Created by TejaDanasvi on 24/7/2023.
+//
+
+import SwiftUI
+
+class ShoeViewModel: ObservableObject {
+    //@Published var usersProducts: Response?
+    @Published var usersProducts = [Products]()
+    
+            
+    init(){
+        print(usersProducts.count)
+        if let localData = self.readLocalFile(forName: "EcommerceJson") {
+            do {
+                let decodedData = try JSONDecoder().decode(ProductModel.self,
+                                                           from: localData)
+                self.usersProducts = decodedData.response?.products ?? []
+            } catch let error {
+                print(error)
+            }
+        }
+    }
+        
+        
+    private func readLocalFile(forName name: String) -> Data? {
+        do {
+            if let bundlePath = Bundle.main.path(forResource: name,
+                                                 ofType: "json"),
+                let jsonData = try String(contentsOfFile: bundlePath).data(using: .utf8) {
+                return jsonData
+            }
+        } catch {
+            print(error)
+        }
+        
+        return nil
+    }
+         
+    }
